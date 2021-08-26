90a92b9574b3856150a3ace59e37ceedd02a92cb
Alexander Konovalov <shuryak@protonmail.com>
2021-08-27 01:30:57 +0300

    initial commit

---
+++ policies/ControllerBypassPolicy.php
@@ -0,0 +1,9 @@
+<?php
+
+class ControllerBypassPolicy implements IActionBypassPolicy
+{
+    public function getClasses()
+    {
+
+    }
+}
---
+++ policies/IActionBypassPolicy.php
@@ -0,0 +1,6 @@
+<?php
+
+interface IActionBypassPolicy
+{
+    function getClasses();
+}
